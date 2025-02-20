101c714437fa24e67ad1537fd89a8879e7cc6c62
samueleangel <samueleangel@gmail.com>
2025-02-20 11:01:41 +0000

    C code fixed (function documentation added)

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ *main - prints the text using puts instead
+ *of printf
+ *Return: 0 if correct, 1 if not.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
