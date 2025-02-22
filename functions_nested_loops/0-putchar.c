c04de720d99d7a55771140cd132f96b52ed67009
XxRedwindxX <samueleangel@gmail.com>
2025-02-22 12:30:44 -0500

    Task 0 added

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Print putchar */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	/* Prints new line */
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This dir will have all my tasks and practices of funcions, and nested loopThis dir will have all my tasks and practices of funcions, and nested loop
