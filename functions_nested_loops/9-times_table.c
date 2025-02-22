14aa36b3dd8b04372a10a460d885ca1a8fb4e84c
samueleangel <samueleangel@gmail.com>
2025-02-22 22:27:16 +0000

    Fix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,18 +3,16 @@
 /**
  * times_table - Prints the 9 times table, starting with 0.
  *
- * Description: This function prints the 9 times table in a  formatted
- * manner.
- * Each number is separated by a comma and two spaces.
+ * Description: This function prints the 9 times table in a formatted manner.
  */
 void times_table(void)
 {
 	int row, column, product;
 
-	/* Travels through each line (0 to 9) */
+	/* Goes through each line (0 to 9) */
 	for (row = 0; row <= 9; row++)
 	{
-		/*Travels through every column (0 to 9) */
+		/* Goes through each column (0 to 9) */
 		for (column = 0; column <= 9; column++)
 		{
 			/* Calculates the product */
@@ -27,7 +25,7 @@ void times_table(void)
 			}
 			else
 			{
-				/* Prints the comma and spaces */
+				/* Prints commas and spaces */
 				_putchar(',');
 				_putchar(' ');
 
@@ -37,12 +35,10 @@ void times_table(void)
 					_putchar(' ');
 				}
 				_putchar('0' + (product / 10));
-
 				_putchar('0' + (product % 10));
 			}
 		}
-	}
 
-	/* Prints a new line at the end of each line*/
-	_putchar('\n');
+		/* Prints a new line at the end of reach line */
+	}
 }
