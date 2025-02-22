0ba9268c5c320fd36bb4c3bc8942006912a9c72f
samueleangel <samueleangel@gmail.com>
2025-02-22 17:56:45 +0000

    Fix

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ *print_alphabet - Prints the alphabet in lowercase, followed by a new line.
  *
- * Description: This function uses _putchar to print each character of the 
- * alphabet
+ *Description: This function uses _putchar to print each character of the
+ *alphabet
  */
 void print_alphabet(void)
 {
