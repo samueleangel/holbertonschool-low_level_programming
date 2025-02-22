35282e910644e00294b502df273020c7f382a1ca
samueleangel <samueleangel@gmail.com>
2025-02-22 18:25:17 +0000

    Fix

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -9,11 +9,10 @@
 int _isalpha(int c)
 {
 	/* Check if c is a letter in lowercase or uppercase */
-	if ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
 	else
 		return (0);
-	}
 }
