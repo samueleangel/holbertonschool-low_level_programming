f2bc0fcc3261db311f8ce955802634e5bdba1ec2
XxRedwindxX <samueleangel@gmail.com>
2025-02-26 13:46:22 -0500

    Fix

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,9 +7,16 @@
  *
  * Return: void
  */
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
+
 void print_numbers(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
 		_putchar(i + '0');
 	}
