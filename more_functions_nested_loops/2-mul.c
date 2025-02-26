198f11f6adf31f5810c512af43911fdc41b44495
XxRedwindxX <samueleangel@gmail.com>
2025-02-26 13:18:41 -0500

    Added multiplication function

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - Multiplies two integers
+ * @a: The first integer to multiply.
+ * @b: The second integer to multiply.
+ *
+ * Description: This function takes two integers, 'a' and 'b',
+ * and returns the result of their multiplication.
+ *
+ * Return: The result of multiplying 'a' and 'b'.
+ */
+int mul(int a, int b)
+{
+	/* Returns the multiplication between a and b */
+	return (a * b);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -6,5 +6,6 @@
 int _isdigit(int c);
 int _putchar(char c);
 int _isupper(int c);
+int mul(int a, int b);
 
 #endif
