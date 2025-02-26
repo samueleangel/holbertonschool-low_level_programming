e20f35c565738534b569d24ab60391662d19f26a
XxRedwindxX <samueleangel@gmail.com>
2025-02-26 13:32:19 -0500

    Function that prints numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
+ *
+ * Description: This function iterates through the numbers 0 to 9 and prints
+ * each one using _putchar. After printing all the numbers it prints a newline.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,5 +7,6 @@ int _isdigit(int c);
 int _putchar(char c);
 int _isupper(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
