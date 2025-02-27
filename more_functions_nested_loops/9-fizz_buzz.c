cb4ac0a88528b4428e6b8f4da474e0d60851a3fa
XxRedwindxX <samueleangel@gmail.com>
2025-02-27 09:23:18 -0500

    Added FizzBuzz exercise

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Description: Prints numbers from 1 to 100, replacing multiples of 3 with "Fizz",
+ * multiples of 5 with "Buzz", and multiples of both 3 and 5 with "FizzBuzz".
+ * Each number or word is separated by a space, and the output ends with a newline.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
