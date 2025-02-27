ec5a01cb0ed1a12f3abaaeed4039c1644ebc7b73
XxRedwindxX <samueleangel@gmail.com>
2025-02-27 09:25:00 -0500

    Fix

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,9 +3,10 @@
 /**
  * main - Entry point of the program.
  *
- * Description: Prints numbers from 1 to 100, replacing multiples of 3 with "Fizz",
- * multiples of 5 with "Buzz", and multiples of both 3 and 5 with "FizzBuzz".
- * Each number or word is separated by a space, and the output ends with a newline.
+ * Description: Prints numbers from 1 to 100, replacing multiples of 3 with
+ * "Fizz", multiples of 5 with "Buzz", and multiples of both 3 and 5 with
+ * "FizzBuzz". Each number or word is separated by a space, and the output
+ * ends with a newline.
  *
  * Return: Always 0 (Success).
  */
