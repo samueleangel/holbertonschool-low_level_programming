56192f919028bb97f20d110ced28ede69bdbe32c
XxRedwindxX <samueleangel@gmail.com>
2025-02-27 10:01:14 -0500

    Fix

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,6 +1,18 @@
 #include <unistd.h>
 
 /**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * print_triangle - Prints a right-aligned triangle using the '#' character.
  * @size: The size of the triangle
  *
