e8548327b2bf70cb73145f0544ceb3fcac43da7d
samueleangel <samueleangel@gmail.com>
2025-03-01 15:26:05 -0500

    Added strncpy_2 fnc

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+/**
+ * _strncpy - Copies a string up to n characters.
+ * @dest: The destination.
+ * @src: Src string to copy.
+ * @n: Maximum number of characters to copy.
+ *
+ * Return: Pointer to the destination string (dest).
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
