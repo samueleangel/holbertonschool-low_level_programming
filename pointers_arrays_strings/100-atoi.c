63e5e2f0ce88e61b12fdab814a9a6fc23dc409f9
samueleangel <samueleangel@gmail.com>
2025-03-01 16:28:30 -0500

    Fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -20,25 +20,25 @@ int _atoi(char *s)
 		}
 		else if (*s == '+')
 		{
-
+			/* Do nothing, sign remains the same */
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
 			digit_found = 1;
-			if (result > (INT_MAX / 10) || (result == (INT_MAX / 10) &&
-			(*s - '0') > (INT_MAX % 10))
+			/* Check for overflow before updating result */
+			if (result > (INT_MAX / 10) || (result == (INT_MAX / 10) && (*s - '0')
+			> (INT_MAX % 10))
 			{
 				return (sign == 1 ? INT_MAX : INT_MIN);
 			}
 			result = result * 10 + (*s - '0');
 		}
+		/* Stop processing if a non-digit is encountered after digits */
+		else if (digit_found)
+		{
+			break;
+		}
+		s++;
 	}
-	else if (digit_found)
-	{
-		break;
-	}
-	s++;
-	}
-
 	return (result * sign);
 }
