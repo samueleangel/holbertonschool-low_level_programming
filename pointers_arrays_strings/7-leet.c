2e23f1b10d586c3a5b202d8cc07806228c9be6d9
samueleangel <samueleangel@gmail.com>
2025-03-01 15:57:04 -0500

    Added leet fnc

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337 (leet) speak.
+ * @str: The string to encode.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char leet_chars[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = leet_chars[j];
+				break; 
+			}
+		}
+	}
+	return (str);
+}
