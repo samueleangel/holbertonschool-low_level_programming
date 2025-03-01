475721a61bf3abe53f4899f9bc1b8052584aa839
samueleangel <samueleangel@gmail.com>
2025-03-01 15:34:08 -0500

    Added strcmp fnc

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ *
+ * Return:
+ * - A negative value if s1 is less than s2.
+ * - 0 if s1 is equal to s2.
+ * - A positive value if s1 is greater than s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
