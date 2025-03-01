1d702325d4dcd3d1d04a6a7d2c253e8072fbc323
samueleangel <samueleangel@gmail.com>
2025-03-01 15:29:13 -0500

    Fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _strncpy - Copies a string up to n characters.
  * @dest: The destination.
@@ -8,7 +10,9 @@
  */
 
 char *_strncpy(char *dest, char *src, int n)
-{
+{	
+	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
