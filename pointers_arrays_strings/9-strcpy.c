7c7052d5ddd345dd6a6efdcae2ff2a184707fbe3
samueleangel <samueleangel@gmail.com>
2025-03-01 10:10:39 -0500

    Added strcpy fnc

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies a string from src to dest, including the null byte.
+ * @dest: A pointer to the destination buffer.
+ * @src: A pointer to the source string.
+ *
+ * Return: A pointer to the destination buffer ('dest').
+ *
+ * Description: This function copies the string pointed to by 'src' (including
+ * the terminating null byte '\0') to the buffer pointed to by 'dest'. It
+ * Returns a pointer to 'dest'.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
