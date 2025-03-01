0b087be1a691a1e75f99c493db23f929de78ef1b
samueleangel <samueleangel@gmail.com>
2025-03-01 16:21:14 -0500

    Fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,44 +2,42 @@
 
 /**
  * _atoi - Converts a string to an integer.
- * @s : A pointer to the string to be converted.
+ * @s: The string to convert.
  *
  * Return: The converted integer.
- *
- * Description: This function converts the string 's' to an integer,
- * an inifnite number of '+' and '-' signs, and stopping at the first
- * non-digit character. If no valid number is found, it returns 0.
  */
 int _atoi(char *s)
 {
-	int sign = 1;
 	int result = 0;
-	int i = 0;
+	int sign = 1;
+	int digit_found = 0;
 
-	while (s[i] == ' ')
+	while (*s)
 	{
-		i++;
-	}
-
-	if (s[i] == '-' || s[i] == '+')
-	{
-		if (s[i] == '-')
+		if (*s == '-')
 		{
-			sign = -1;
+			sign *= -1;
 		}
-		i++;
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (result > (INT_MAX / 10) || (result == (INT_MAX / 10)
-		&& (s[i] - '0') > (INT_MAX % 10)))
+		else if (*s == '+')
 		{
-			return (0);
-		}
 
-		result = result * 10 + (s[i] - '0');
-		i++;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit_found = 1;
+			if (result > (INT_MAX / 10) || (result == (INT_MAX / 10) &&
+			(*s - '0') > (INT_MAX % 10))
+			{
+				return (sign == 1 ? INT_MAX : INT_MIN);
+			}
+			result = result * 10 + (*s - '0');
+		}
+	}
+	else if (digit_found)
+	{
+		break;
+	}
+	s++;
 	}
 
 	return (result * sign);
