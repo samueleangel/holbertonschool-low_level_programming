7f1ce113324287d8d3d2c77588720183109f04d9
samueleangel <samueleangel@gmail.com>
2025-03-06 20:53:14 +0100

    Added str fnc

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Pointer to the main string.
+ * @accept: Pointer to the substring containing accepted characters.
+ *
+ * Return: Number of bytes in the initial segment of s consisting only of bytes from accept.
+ */
+unsigned int _ strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *a;
+
+	while (*s)
+	{
+		for (a = accept; *a; a++)
+		{
+			if (*s == *a)
+			{
+				count++;
+				break;
+			}
+		}
+		if (!*a)
+			return(count);
+		s++;
+	}
+	return (count);
+}
