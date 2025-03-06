208a735e631e49c0fbaa81d68f0fbf6bbdb3a2b2
samueleangel <samueleangel@gmail.com>
2025-03-06 20:59:17 +0100

    Added str searcher fnc

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: Pointer to the main string.
+ * @accept: Pointer to the substring containing accepted bytes.
+ *
+ * Return: Pointer to the first occurrence in s of any byte in accept, or NULL if not found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+
+	while (*s)
+	{
+		for (a = accept; *a; a++)
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			s++;
+		}
+		return (NULL);
+	}
+}
