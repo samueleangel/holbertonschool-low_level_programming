07faf1cd10c6f8faf5ac9c25bfb1e4bba04e495d
samueleangel <samueleangel@gmail.com>
2025-03-06 20:21:28 +0100

    Added another memory copying fnc

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory area.
+ * @dest: Pointer to the destination memory area. 
+ * @src: Pointer to the source memory area.
+ * @n: The number of bytes to copy.
+ *
+ * Return: Pointer to the destination memory area dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
