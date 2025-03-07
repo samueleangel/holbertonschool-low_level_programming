e87796d5ae7cb6f8c9f756bf7463d2a86f8b15b1
samueleangel <samueleangel@gmail.com>
2025-03-07 22:26:29 +0100

    Added fnc rtrn square *2

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * find_sqrt - Helper function to find the square root recursively.
+ * @n: The number to find the square root of.
+ * @guess: The current guess for the square root.
+ *
+ * Return: The natural square root, or -1 if none exists.
+ */
+int find_sqrt(int n, int guess)
+{
+	if (guess * guess > n)
+	{
+		return (-1);
+	}
+	if (guess * guess == n)
+		return (guess);
+	return (find_sqrt(n, guess + 1));
+}
