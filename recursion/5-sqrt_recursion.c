412bf2c6b7d2311459ad2606dce89489990dd938
samueleangel <samueleangel@gmail.com>
2025-03-07 22:35:02 +0100

    Fix

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -7,13 +7,26 @@
  *
  * Return: The natural square root, or -1 if none exists.
  */
-int _sqrt_recursion(int n)
+int find_sqrt(int n, int guess)
 {
 	if (guess * guess > n)
 	{
 		return (-1);
+		if (guess * guess == n)
+			return (guess);
+		return (find_sqrt(n, guess + 1));
 	}
-	if (guess * guess == n)
-		return (guess);
-	return (find_sqrt(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to compute the square root of.
+ *
+ * Return: The natural square root, or -1 if none exists.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (find_sqrt(n, 0));
 }
--- recursion/main.h
+++ recursion/main.h
@@ -6,5 +6,6 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
+int find_sqrt(int n, int guess);
 int _sqrt_recursion(int n);
 #endif
