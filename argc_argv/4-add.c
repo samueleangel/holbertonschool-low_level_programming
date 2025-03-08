dc6ad4ffc51cbfae49cb57a6c14bf40952fbd4fa
samueleangel <samueleangel@gmail.com>
2025-03-08 22:17:54 +0100

    added argc fnc

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Adds positive numbers
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argc[])
+{
+	int sum = 0;
+	int i, j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argc[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
