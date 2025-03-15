3da526bb6c4e464bb495b4d648bac46741b22f40
samueleangel <samueleangel@gmail.com>
2025-03-15 19:10:06 +0100

    Fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
 #include <stdio.h>
-
 /**
  * struct dog - Structure to represent a dog.
  * @name: Dog´s name (string).
@@ -14,6 +15,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}; 
+};
 
-typedef struct dog dog_t;
+/* Prototypes */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
