244f62ceac70a05f2a8c4eff6760571aded79c47
samueleangel <samueleangel@gmail.com>
2025-03-15 16:56:58 +0100

    Added mallc check fnc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to allocate.
+ *
+ * Return: A pointer to the allocated memory.
+ * If malloc fails, the function terminates the process with status 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "malloc failed\n");
+		exit(98);
+	}
+
+	return (ptr);
+}
---
+++ more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+void *malloc_checked(unsigned int b);
+
+#endif
