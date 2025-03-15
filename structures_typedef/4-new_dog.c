c525ec1f4aa6cefa8d21431e8d2a942f16d61a3d
samueleangel <samueleangel@gmail.com>
2025-03-15 22:33:38 +0100

    Added fnc new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include "dog.h"
+#include <string.h>
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: A pointer to the newly created dog_t, or NULL if the function fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *name_copy, *owner_copy;
+
+	new_dog = (dog_t *)malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	name_copy = (char *)malloc(strlen(name) + 1);
+	if (name_copy == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(name_copy, name);
+
+	owner_copy = (char *)malloc(strlen(owner) + 1);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(owner_copy, owner);
+
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+
+	return (new_dog);
+}
+
