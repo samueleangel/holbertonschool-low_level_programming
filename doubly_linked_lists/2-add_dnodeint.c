f6c87db76b43b6d091d9edffb7817416c7204f6d
samueleangel <samueleangel@gmail.com>
2025-04-05 14:42:39 +0100

    Fix

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
-	if(new_node == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
