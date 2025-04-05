0b74704d4d90cb7a4c912837ae076c18bafc678c
samueleangel <samueleangel@gmail.com>
2025-04-05 16:05:53 +0100

    Added binary printer fnc

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -10,6 +11,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
+int _putchar(char c);
 
 #endif
