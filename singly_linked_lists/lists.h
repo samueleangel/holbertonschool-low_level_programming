4a03303723c065fb780423c9bc84f45d4837d143
samueleangel <samueleangel@gmail.com>
2025-04-05 13:30:58 +0100

    Fix

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,10 +1,10 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#ifndef LISTS_H
-#define LISTS_H
 
-size_t print_list(const list_t *h);
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
