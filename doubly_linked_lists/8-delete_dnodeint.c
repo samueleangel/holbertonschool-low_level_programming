ab89e4cb05940edf4d113e754e5600f15c98c721
samueleangel <samueleangel@gmail.com>
2025-04-05 15:20:31 +0100

    fnc end-node deleter

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t
+ * list.
+ * @head: pointer to the head of the list.
+ * @index: the index of the node to be deleted.
+ *
+ * Return: 1 if it succeded, -1 if it failed.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
+	unsigned int i = 0;
+
+	if (current == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+
+	free(current);
+	return (1);
+}
