e9c83210c7de67f4a94ff82c0e13ae406fe8623c
samueleangel <samueleangel@gmail.com>
2025-04-05 21:14:03 +0100

    Added cp program

---
+++ file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * main- Copies the content of a file to another file.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, read_bytes, write_bytes;
+	char buffer[BUF_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can´t read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
+		| S_IWUSR | S_IRGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can´t write to file %s\n", argv[2]);
+		close(fd_from);
+		exit(99);
+	}
+
+	while ((read_bytes = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		write_bytes = write(fd_to, buffer, read_bytes);
+		if (write_bytes == -1 || write_bytes != read_bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: can´t write to file %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+
+	if (read_bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can´t read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	if (close(fd_to) == 1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can´t close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -5,6 +5,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
